e1948930ffcf0281b77a256de55382f77cb9eabb
gildonghyeon <118823021+gildonghyeon@users.noreply.github.com>
2023-05-13 22:31:44 +0900

    Add files via upload

---
+++ 오픈소스/index.py
@@ -0,0 +1,12 @@
+txt = input('문자열 txt: ')
+ptn = input('문자열 ptn: ')
+
+c = txt.count(ptn)
+
+if c == 0:                                                 
+    print('ptn은 txt에 포함되어 있지 않습니다.')
+elif c == 1:                                             
+    print('ptn이 txt에 포함되어 있는 인덱스: ', txt.index(ptn))
+else:                                                      
+    print('ptn이 txt에 포함되어 있는 맨 앞 인덱스: ', txt.index(ptn))
+    print('ptn이 txt에 포함되어 있는 맨 끝 인덱스: ', txt.rindex(ptn))
